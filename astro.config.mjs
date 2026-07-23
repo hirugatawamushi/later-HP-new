11ec804717a6623b2c4b3714de2bd4fcb14b8ec9
hirugatawamushi <hirugata.wamushi8@gmail.com>
2026-07-23 23:03:30 +0900

    background-change

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,13 +4,13 @@ import { loadEnv } from 'vite';
 
 import vercel from '@astrojs/vercel';
 
-const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
+// const env = loadEnv(process.env.NODE_ENV || 'development', process.cwd(), '');
 
 export default defineConfig({
   site: 'https://my-portfolio.vercel.app',
   integrations: [
     storyblok({
-      accessToken: env.STORYBLOK_TOKEN,
+      accessToken: process.env.STORYBLOK_TOKEN,
       devToolbar: false,
       components: {
         page: 'storyblok/Page',
